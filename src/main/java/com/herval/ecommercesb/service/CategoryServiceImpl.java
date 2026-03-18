67a19dee4be69410df23d034eb8ab0dede524b10
Herval Mata <hervalmataamparo@gmail.com>
2026-03-18 16:19:07 -0300

    :sparkles: feat: ecommerce - category jpa and validation

--- src/main/java/com/herval/ecommercesb/service/CategoryServiceImpl.java
+++ src/main/java/com/herval/ecommercesb/service/CategoryServiceImpl.java
@@ -1,52 +1,50 @@
 package com.herval.ecommercesb.service;
 
 import com.herval.ecommercesb.model.Category;
+import com.herval.ecommercesb.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-    private List<Category> categories = new ArrayList<>();
+
     private Long nextId = 1L;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada"));
 
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Categoria com Id: " + categoryId + " foi removida com sucesso";
     }
 
     @Override
     public Category updateCategory(Long categoryId, Category category) {
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada"));
 
-        if (optionalCategory.isPresent()) {
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
-        } else  {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada");
-        }
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
     }
 }
